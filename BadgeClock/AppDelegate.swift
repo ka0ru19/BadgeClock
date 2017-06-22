7c2a419123b1d3452b062069fecfd11f4bd8e0d8
ka0ru19 <k.prfm19@gmail.com>
2017-06-22 16:57:51 +0900

    最初のcommit

--- BadgeClock/AppDelegate.swift
+++ BadgeClock/AppDelegate.swift
@@ -7,18 +7,22 @@
 //
 
 import UIKit
+import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        requestNotificationPermission()
+        
         return true
     }
 
+    //バックグラウンド遷移移行直前に呼ばれる
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
@@ -41,6 +45,28 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
+    //アプリがアクティブになる度に呼ばれる
+    func applicationDidBecomeActive(application: UIApplication) {
+    }
+
 
 }
 
+extension AppDelegate {
+    func requestNotificationPermission() {
+        let center:UNUserNotificationCenter = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.badge], completionHandler: {(permit, error) in
+            
+            if permit {
+                print("通知が許可されました")
+            }else {
+                print("通知が拒否されました")
+            }
+            
+            //バッジの数の設定.
+//            UIApplication.shared.applicationIconBadgeNumber = 1 // テスト
+            
+        })
+    }
+
+}
---
+++ BadgeClock/ClockViewController.swift
@@ -0,0 +1,134 @@
+//
+//  ClockViewController.swift
+//  BadgeClock
+//
+//  Created by Wataru Inoue on 2017/06/21.
+//  Copyright © 2017年 Wataru Inoue. All rights reserved.
+//
+
+import UIKit
+
+class ClockViewController: UIViewController {
+    
+    let datePicker = UIDatePicker() //DatePickerオブジェクト生成
+    let displaySwitch = UISwitch() // 表示するかどうかのswitch
+    //    let dateFomatter = DateFormatter() // datePicker -> limitDate
+    let date2badgeFomatter = DateFormatter() // 現在時刻 -> Badgeに表示
+    var backgroundTaskIdentifier: UIBackgroundTaskIdentifier = 0
+    
+    var timer = Timer()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        
+        initNotification()
+        initInstances()
+        initView()
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        <#code#>
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
+extension ClockViewController {
+    
+    func initNotification() {
+        // backgroundに移行した時に呼ばれるメソッド // backgroundは基本的に3分間
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(self.displayBadgeClock),
+            name: NSNotification.Name.UIApplicationWillResignActive,
+            object: nil
+        )
+        
+        // アプリがアクティブになった時に呼ばれるメソッド
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(self.stopBackgroundClock),
+            name: NSNotification.Name.UIApplicationDidBecomeActive,
+            object: nil
+        )
+    }
+    
+    func initInstances() {
+        
+        self.tabBarItem.badgeColor = UIColor.red
+        
+        displaySwitch.isOn = false
+        displaySwitch.addTarget(self, action: #selector(self.onClickSwicth(sender:)), for: UIControlEvents.valueChanged)
+        
+        datePicker.timeZone = NSTimeZone.local
+        datePicker.datePickerMode = .dateAndTime
+        datePicker.addTarget(self, action: #selector(self.onClickSwicth(sender:)), for: .valueChanged)
+        
+        date2badgeFomatter.dateFormat = "ss" // Badgeに表示するフォーマット
+        date2badgeFomatter.timeZone = NSTimeZone.local
+        
+    }
+    
+    func initView() {
+        /****** baseのview ******/
+        let baseView = UIView(frame: CGRect(x: 0, y: 0, width: self.view.bounds.width, height: self.view.bounds.height))
+        
+        displaySwitch.layer.position = CGPoint(x: baseView.frame.width * 3 / 4, y: baseView.frame.height * 1 / 3)
+        
+        datePicker.frame = CGRect(x: 0, y: 0, width: baseView.frame.width, height: 200)
+        datePicker.frame.origin.y = baseView.bottomY - DeviceSize.tabBarHeight(tabBarController: self.tabBarController!) - datePicker.frame.height
+        
+        /****** view の統合 ******/
+        baseView.addSubview(displaySwitch)
+        baseView.addSubview(datePicker)
+        self.view.addSubview(baseView)
+    }
+    
+    func onClickSwicth(sender: UISwitch) {
+        print(sender.isOn)
+        
+        if sender.isOn {
+            self.tabBarItem.badgeValue = "ON"
+        } else {
+            self.tabBarItem.badgeValue = nil
+        }
+    }
+    
+    func onDidChangeDate(sender: UIDatePicker) {
+        //        print(dateFomatter.string(from: sender.date))
+    }
+    
+    func displayBadgeClock() {
+        if displaySwitch.isOn {
+            backgroundTaskIdentifier = UIApplication.shared.beginBackgroundTask {
+                UIApplication.shared.endBackgroundTask(self.backgroundTaskIdentifier)
+            }
+            timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateClock), userInfo: nil, repeats: true)
+        } else {
+            if timer.isValid {
+                timer.invalidate()
+            }
+            UIApplication.shared.applicationIconBadgeNumber = 0
+        }
+    }
+    
+    func updateClock() {
+        let str = date2badgeFomatter.string(from: Date().addingTimeInterval(TimeInterval(1))) // 1secだけ先取りする
+        print("\(Date()) ->  \(str)")
+        if let numClock = Int(str) {
+            UIApplication.shared.applicationIconBadgeNumber = numClock
+        }
+    }
+    
+    func stopBackgroundClock() {
+        if timer.isValid {
+            timer.invalidate()
+        }
+    }
+}
