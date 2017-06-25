1a98bad16cdd75a3aa97eeab78ac7bee69521cd9
ka0ru19 <k.prfm19@gmail.com>
2017-06-25 23:10:01 +0900

    リファクタリング

--- BadgeClock/AppDelegate.swift
+++ BadgeClock/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        requestNotificationPermission()
+        requestNotificationPermission() // 通知のBadgeを利用してもいいかの許可を求める
         
         return true
     }
@@ -62,9 +62,6 @@ extension AppDelegate {
             }else {
                 print("通知が拒否されました")
             }
-            
-            //バッジの数の設定.
-//            UIApplication.shared.applicationIconBadgeNumber = 1 // テスト
             
         })
     }
